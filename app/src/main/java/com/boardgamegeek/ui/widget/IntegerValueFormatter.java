d90b4b1e948bf973ee25c34c10e818949b49c9da
Chris Comeaux <chris.comeaux@gmail.com>
2016-10-22 21:07:35 -0500

    Start upgrading

--- app/src/main/java/com/boardgamegeek/ui/widget/IntegerValueFormatter.java
+++ app/src/main/java/com/boardgamegeek/ui/widget/IntegerValueFormatter.java
@@ -1,12 +1,12 @@
 package com.boardgamegeek.ui.widget;
 
 import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.formatter.IValueFormatter;
 import com.github.mikephil.charting.utils.ViewPortHandler;
 
 import java.text.DecimalFormat;
 
-public class IntegerValueFormatter implements ValueFormatter {
+public class IntegerValueFormatter implements IValueFormatter {
 	private final DecimalFormat format;
 	private final boolean shouldSuppressZero;
 
--- app/src/main/java/com/boardgamegeek/ui/widget/IntegerYAxisValueFormatter.java
+++ app/src/main/java/com/boardgamegeek/ui/widget/IntegerYAxisValueFormatter.java
@@ -1,11 +1,11 @@
 package com.boardgamegeek.ui.widget;
 
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.formatter.YAxisValueFormatter;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 
 import java.text.DecimalFormat;
 
-public class IntegerYAxisValueFormatter implements YAxisValueFormatter {
+public class IntegerYAxisValueFormatter implements IAxisValueFormatter {
 	private final DecimalFormat format;
 
 	public IntegerYAxisValueFormatter() {
@@ -13,7 +13,7 @@ public class IntegerYAxisValueFormatter implements YAxisValueFormatter {
 	}
 
 	@Override
-	public String getFormattedValue(float value, YAxis yAxis) {
+	public String getFormattedValue(float value, AxisBase axis) {
 		return format.format(value);
 	}
 }
